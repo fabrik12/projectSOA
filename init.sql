4a59aa2d4a74fcc56b96801827b2cabd145f1124
fabrik12 <fbalarezo@unsa.edu.pe>
2025-06-21 11:07:45 -0500

    Añadiendo archivos de configuración para Docker y script de inicialización de base de datos

---
+++ init.sql
@@ -0,0 +1,16 @@
+-- init.sql
+CREATE TABLE IF NOT EXISTS productos (
+    id VARCHAR(50) PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion TEXT,
+    categoria VARCHAR(255) NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL
+    --stock INT NOT NULL DEFAULT 0
+);
+
+-- Opcional: Insertar algunos datos de ejemplo para probar
+INSERT INTO productos (id, nombre, descripcion, categoria, precio, stock) VALUES
+('P001', 'Laptop Gamer', 'Potente laptop para juegos', 'Laptops',1500.00),
+('P002', 'Teclado Mecánico', 'Teclado RGB con switches azules', 'Periferico',80.50),
+('P003', 'Mouse Inalámbrico', 'Mouse ergonómico para oficina', 'Periferico',35.00)
+ON CONFLICT (id) DO NOTHING; 
